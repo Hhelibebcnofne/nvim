7e1d44ae0931e595a5c117e0879892a01b8972bd
Hhelibebcnofne <29158192+Hhelibebcnofne@users.noreply.github.com>
2026-04-03 00:29:35 +0900

    feat: Add markdown-preview

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # nvim
 
 This is my nvim config.
+
+:Lazy load markdown-preview.nvim
+:Lazy build markdown-preview.nvim
--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,4 +3,7 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+    init = function()
+        vim.g.mkdp_echo_preview_url = 1
+    end,
 }
