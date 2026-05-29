7d38ac1595fa145057cacb41932a69c6eb3c5df5
Hhelibebcnofne <29158192+Hhelibebcnofne@users.noreply.github.com>
2026-05-29 21:51:29 +0900

    feat: change telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-	"nvim-telescope/telescope.nvim",
+	"https://github.com/nvim-telescope/telescope.nvim",
 	event = "VeryLazy",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
@@ -7,9 +7,9 @@ return {
 	opts = {},
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>tf", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>tg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>tb", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>th", builtin.help_tags, { desc = "Telescope help tags" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 	end,
 }
