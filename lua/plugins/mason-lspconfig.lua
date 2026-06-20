784c8b19bf2ebec10a214b46e246f7356a877be8
Hhelibebcnofne <29158192+Hhelibebcnofne@users.noreply.github.com>
2026-06-21 01:35:14 +0900

    feat: update lsp settings

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,7 @@
+-- @type LazyPluginSpec
 return {
-	"https://github.com/mason-org/mason.nvim",
-	opts = {},
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	opts = { automatic_enable = true },
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,4 @@
+-- @type LazyPluginSpec
 return {
 	"https://github.com/neovim/nvim-lspconfig",
 	event = { "BufNewFile", "BufReadPre" },
